0449a2e7446eeed2150874adaada7a09709f0c02
Kanchan Kumar Tiwari <coolkanchan13@gmail.com>
2019-09-09 17:47:28 +0530

    updated db.users.js,removed my credentials

--- backend/databases/db.users.js
+++ backend/databases/db.users.js
@@ -3,8 +3,8 @@ const mySQL = require("mysql");
 const connection = mySQL.createConnection({
     host:"localhost",
     user:"root",
-    password:"New@12345",
-    database:"user",
+    password:"Password_Of_My_Sql",
+    database:"Database_Name",
     });
     
  connection.connect(function(err){
